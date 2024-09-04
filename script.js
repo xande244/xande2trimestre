d8e8e8280f14b32106d087854cd129e02e6b420a
alexandre <118493869+xande244@users.noreply.github.com>
2024-09-04 19:37:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Oque vem em sua cabeça sobre isso?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "Isso é aterrorizante!",
                 afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "Isso é incrível!",
                 afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
